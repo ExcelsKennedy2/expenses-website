e309d8746d0b2f159fa38a4f94a1100c8d940d6a
ExcelsKennedy2 <kennedymbugua841@gmail.com>
2025-02-25 13:00:31 +0300

    updated registration section

--- expensesWebsite/settings.py
+++ expensesWebsite/settings.py
@@ -141,3 +141,4 @@ EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL')
 EMAIL_PORT = 587
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
